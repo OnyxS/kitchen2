c56434093c83450d2d385dba5a5dd364f75a034b
OnyxS <64073279+OnyxS@users.noreply.github.com>
2021-05-29 07:03:33 +0300

    finish

---
+++ js/script.js
@@ -0,0 +1,64 @@
+var btnContainer = document.getElementById("works__header-btns");
+
+var gallery = document.getElementById("works__slider-wrapper");
+
+var img=gallery.getElementsByClassName("works__slider-slide")
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("works__header-button");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+
+  });
+}
+
+
+
+
+
+const worksSlider=new Swiper('.works__slider',{
+	
+	pagination:{
+		el:'.swiper-pagination',
+		clickable:true,
+		slidesPerView: 'auto',
+	},
+});
+
+$('.button_1').on('click', function() {
+	$('.studia').css('display','none')
+	$('.last').css('display','block')
+	worksSlider.update();
+});
+
+$('.button_2').on('click', function() {
+	$('.studia').css('display','block')
+	$('.last').css('display','none')
+	worksSlider.update();
+});
+
+const commentSlider=new Swiper('.comment__slider',{
+	
+	pagination:{
+		el:'.swiper-pagination',
+		clickable:true,
+		slidesPerView: 'auto',
+		spaceBetween:'300',
+	},
+});
+
+function res(){
+   var width = $('.slide__row-item').width();
+	 $('.slide__row').height(width);
+} res();
+
+$( window ).resize(function() {
+	res();
+});
+
+$('.mystyle').css('margin','10px')
